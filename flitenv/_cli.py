5bcfb030891d22a2800c32c61471b0ee60c72076
gram <mail@orsinium.dev>
2021-07-02 11:16:57 +0200

    support envs for lock

--- flitenv/_cli.py
+++ flitenv/_cli.py
@@ -1,32 +1,21 @@
-import subprocess
 import sys
 import typing
 from argparse import ArgumentParser
 from pathlib import Path
 
-from ._core import Env
+import toml
+
+from ._core import Env, MAIN_ENV
 
 
 def cmd_lock(args, stream: typing.TextIO) -> int:
-    tmp = Path('_tmp_requirements.in')
-    if args.constraint:
-        tmp.write_text(f'-c {args.constraint}')
-    cmd = [
-        sys.executable, '-m', 'piptools', 'compile',
-        '--annotate',
-        '--generate-hashes',
-        '--no-header',
-        '--no-emit-index-url',
-        '--output-file', 'requirements.txt',
-        'pyproject.toml',
-    ]
-    if args.constraint:
-        cmd.append(str(tmp))
-
-    result = subprocess.run(cmd)
-    if args.constraint:
-        tmp.unlink()
-    return result.returncode
+    env = Env(
+        name=args.env,
+        root=args.root,
+        venvs=args.venvs,
+        stream=stream,
+    )
+    return env.lock(constraint=args.constraint)
 
 
 def cmd_install(args, stream: typing.TextIO) -> int:
@@ -49,37 +38,52 @@ def cmd_run(args, stream: typing.TextIO) -> int:
     return env.run(args.exe, *args.args)
 
 
+def get_envs() -> typing.Optional[typing.List[str]]:
+    path = Path('pyproject.toml')
+    if not path.exists():
+        return None
+    with path.open('r', encoding='utf-8') as stream:
+        pyproject = toml.load(stream)
+    try:
+        meta = pyproject['tool']['flit']['metadata']
+    except KeyError:
+        return None
+    try:
+        envs = meta['requires-extra']
+    except KeyError:
+        return [MAIN_ENV]
+    return [MAIN_ENV] + sorted(envs)
+
+
 def main(argv: typing.List[str], stream: typing.TextIO) -> int:
 
     parser = ArgumentParser()
     parser.format_help
-    parser.add_argument('env')
+    parser.add_argument('env', choices=get_envs())  # type: ignore
+    parser.add_argument('--root', type=Path, default=Path())
+    parser.add_argument('--venvs', default='.venvs')
     subparsers = parser.add_subparsers()
+
+    lock_parser = subparsers.add_parser(
+        name='lock',
+        help='lock env dependencies',
+    )
+    lock_parser.add_argument('-c', '--constraint')
+    lock_parser.set_defaults(func=cmd_lock)
 
     install_parser = subparsers.add_parser(
         name='install',
         help='install env dependencies',
     )
-    install_parser.add_argument('--root', type=Path, default=Path())
-    install_parser.add_argument('--venvs', default='.venvs')
     install_parser.set_defaults(func=cmd_install)
 
     run_parser = subparsers.add_parser(
         name='run',
         help='run a command inside the env',
     )
-    run_parser.add_argument('--root', type=Path, default=Path())
-    run_parser.add_argument('--venvs', default='.venvs')
     run_parser.add_argument('exe')
     run_parser.add_argument('args', nargs='...')
     run_parser.set_defaults(func=cmd_run)
-
-    lock_parser = subparsers.add_parser(
-        name='lock',
-        help='generate lock file',
-    )
-    lock_parser.add_argument('-c', '--constraint')
-    lock_parser.set_defaults(func=cmd_lock)
 
     args = parser.parse_args(argv)
     return args.func(args=args, stream=stream)
--- flitenv/_core.py
+++ flitenv/_core.py
@@ -7,6 +7,9 @@ import flit
 from dephell_venvs import VEnv
 
 
+MAIN_ENV = 'main'
+
+
 class Env(typing.NamedTuple):
     root: Path
     name: str
@@ -24,6 +27,13 @@ class Env(typing.NamedTuple):
         result = subprocess.run(cmd, stdout=subprocess.DEVNULL)
         result.check_returncode()
 
+    @property
+    def constraints(self) -> Path:
+        name = 'requirements.txt'
+        if self.name != MAIN_ENV:
+            name = f'requirements-{self.name}.txt'
+        return self.root / name
+
     def install(self) -> int:
         venv = VEnv(path=self.path)
         if not venv.exists():
@@ -35,7 +45,7 @@ class Env(typing.NamedTuple):
         if not (bin_path / 'wheel').exists():
             print('installing wheel...', file=self.stream)
             self._pip_install('pip', 'wheel', 'setuptools')
-        constr = self.root / 'requirements.txt'
+        constr = self.constraints
         if constr.exists():
             print('installing requirements.txt...', file=self.stream)
             self._pip_install('-r', str(constr))
@@ -47,7 +57,7 @@ class Env(typing.NamedTuple):
                 '--deps', 'production',
                 '--symlink',
             ]
-            if self.name != 'prod':
+            if self.name != MAIN_ENV:
                 cmd.extend(['--extras', self.name])
             print('installing project deps...', file=self.stream)
             flit.main(cmd)
@@ -69,4 +79,27 @@ class Env(typing.NamedTuple):
         full_cmd = [str(bin_path / exe)]
         full_cmd.extend(cmd)
         result = subprocess.run(full_cmd)
+        return result.returncode
+
+    def lock(self, constraint: str) -> int:
+        tmp = Path('_tmp_requirements.in')
+        if constraint:
+            tmp.write_text(f'-c {constraint}')
+        cmd = [
+            sys.executable, '-m', 'piptools', 'compile',
+            '--annotate',
+            '--generate-hashes',
+            '--no-header',
+            '--no-emit-index-url',
+            '--output-file', 'requirements.txt',
+            'pyproject.toml',
+        ]
+        if self.name != MAIN_ENV:
+            cmd.extend(['--extra', self.name])
+        if constraint:
+            cmd.append(str(tmp))
+
+        result = subprocess.run(cmd)
+        if constraint:
+            tmp.unlink()
         return result.returncode
